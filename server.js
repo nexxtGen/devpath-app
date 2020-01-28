7c7bde3f7ac66732fb987f35fcb807f6ecd056e6
Kamil Szczypior <kszczypior1@gmail.com>
2020-01-28 14:32:07 +0100

    B: add connection with DB via Mongoose

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
-// Route files
-const flashcards = require('./routes/flashcards');
-
-// Load env vars
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+const flashcards = require('./routes/flashcards');
 
 const app = express();
 
-// Mount routes
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use('/api/v1/flashcards', flashcards);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
