a82b90d4661dd81a0b28d6426bdf3ee3d882f58b
Kamil Szczypior <kszczypior1@gmail.com>
2020-01-31 22:49:04 +0100

    B: add User register and encrypting password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,27 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
+const gravatar = require('gravatar');
 
 // @desc Register user
 // @route GET /api/v1/auth/register
 // @access Public
 
 exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+  const avatar = gravatar.url(email, {
+    s: '200',
+    r: 'pg',
+    d: 'mm'
+  });
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+    avatar
+  });
+
   res.status(200).json({ success: true });
 });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -8,6 +9,7 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Please add an email'],
+    unique: true,
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       'Please add a valid email'
@@ -33,6 +35,11 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+UserSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model('User', UserSchema);
