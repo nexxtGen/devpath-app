5462999c1cc0ab4c2d0efd98582df15042f2a17a
Kamil Szczypior <kszczypior1@gmail.com>
2020-01-31 23:43:16 +0100

    B: add User login

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,9 +4,8 @@ const User = require('../models/User');
 const gravatar = require('gravatar');
 
 // @desc Register user
-// @route GET /api/v1/auth/register
+// @route POST /api/v1/auth/register
 // @access Public
-
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
   const avatar = gravatar.url(email, {
@@ -23,6 +22,35 @@ exports.register = asyncHandler(async (req, res, next) => {
     avatar
   });
 
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
+
+// @desc Login user
+// @route POST /api/v1/auth/login
+// @access Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if pass matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Create token
   const token = user.getSignedJwtToken();
 
   res.status(200).json({ success: true, token });
