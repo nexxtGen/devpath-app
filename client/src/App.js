1aac906604b5fcb03e007a536f3b17b0d76184bd
Kamil Szczypior <kszczypior1@gmail.com>
2020-02-03 10:44:37 +0100

    F: init React, cleaning up, initial components

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import LandingPage from './components/layout/LandingPage';
+import './App.css';
+
+const App = () => (
+  <Fragment>
+    <LandingPage />
+  </Fragment>
+);
+
+export default App;
