1e81d4bc0d6736ef7a2a4810ac700d45a478a902
Kamil Szczypior <kszczypior1@gmail.com>
2020-02-03 07:53:31 +0100

    B: sending JWT in a cookie

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -15,6 +16,7 @@ const auth = require('./routes/auth');
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
