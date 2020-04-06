425502f4c08e91e8c27142749984ab02c6fbeb9a
Kamil Szczypior <kszczypior1@gmail.com>
2020-04-06 22:55:39 +0200

    B: create lane schema

---
+++ models/Lane.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const LaneSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  name: {
+    type: String,
+    required: [true, 'Please add a lane name'],
+    trim: true,
+    maxlength: [50, 'Lane name can not be more than 50 characters']
+  },
+  notes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Note',
+      required: true
+    }
+  ],
+  boardId: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Lane', LaneSchema);
