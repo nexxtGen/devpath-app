ded9d0b5dcbbe307bb4c0984c968f7be2fa5fc9f
Kamil Szczypior <kszczypior1@gmail.com>
2020-04-07 23:19:29 +0200

    B: create note schema

---
+++ models/Note.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add a note description'],
+    trim: true,
+    maxlength: [250, 'Note name can not be more than 250 characters']
+  },
+  priority: {
+    type: String,
+    required: true,
+    enum: ['low', 'medium', 'important']
+  },
+  LaneId: {
+    type: String,
+    required: true
+  },
+  boardId: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Note', NoteSchema);
