d335c958da4e17bc60b3da21d1269ed1bc3819a3
Kamil Szczypior <kszczypior1@gmail.com>
2020-05-02 19:37:10 +0200

    D: test deploy 02

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const colors = require('colors');
 const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
+const path = require('path');
 
 dotenv.config({ path: './config/config.env' });
 
@@ -30,6 +30,14 @@ app.use(cookieParser());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
+}
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 // Mount Routes
