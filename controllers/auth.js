32618e156b543791a6985f462c698bcf4b91e7e9
Kamil Szczypior <kszczypior1@gmail.com>
2020-05-03 01:57:12 +0200

    D: change secure http otpion

--- controllers/auth.js
+++ controllers/auth.js
@@ -60,7 +60,7 @@ const sendTokenResponse = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true
+    httpOnly: false
   };
 
   if (process.env.NODE_ENV === 'production') {
